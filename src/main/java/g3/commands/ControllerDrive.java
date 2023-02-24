8ed1675bb21e7985a7c56f1f45e606c9b9d38ea0
liad-inon <liadhbui@gmail.com>
2023-02-24 18:05:26 +0200

    added tolerance in the axes?

--- src/main/java/g3/commands/ControllerDrive.java
+++ src/main/java/g3/commands/ControllerDrive.java
@@ -26,13 +26,30 @@ public class ControllerDrive extends CommandBase {
         double velocityMul = (controller.inst.getRightTriggerAxis() < 0.9 && controller.inst.getRightTriggerAxis() < 0.9) ? 1:0.3;
         speedVal = stickLeft[1]*velocityMul;
         
-         drive.tankDrive(controller.stickControlFunc(Math.pow((stickLeft[1]*velocityMul),1.5)+0.3), 
-                 controller.stickControlFunc(Math.pow((stickRight[1]*velocityMul),1.5)+0.3)); // added function multiplier
-
-        //drive.tankDrive(stickLeft[1],(stickRight[1])); // added function multiplier
-
+        //drive.tankDrive(controller.stickControlFunc(Math.pow((stickLeft[1]*velocityMul),1.5)+0.3), 
+       //          controller.stickControlFunc(Math.pow((stickRight[1]*velocityMul),1.5)+0.3)); // added function multiplier
+        if ((controller.getStickLeft()[1]>0.1 || controller.getStickLeft()[1]<0.1) && (controller.getStickRight()[5]>0.1 || controller.getStickRight()[5]<0.1)){
+        drive.tankDrive(stickLeft[1],(stickRight[1])); // added function multiplier
+        }
+        else if  ((controller.getStickLeft()[1]>0.1 || controller.getStickLeft()[1]<0.1)){
+                drive.tankDrive(stickLeft[1],0); // added function multiplier
+                }
+        
+        else if  ((controller.getStickRight()[5]>0.1 || controller.getStickRight()[5]<0.1)){
+            drive.tankDrive(0,stickRight[1]); // added function multiplier
+            }
+        else {
+            drive.tankDrive(0,0);
+        }
         
     }
+
+
+            
+    
+
+        
+    
 
     @Override
     public boolean isFinished() {
